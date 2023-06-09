a368501e26858e5c55d146d22884e0744d9cb31c
s1300037 <s1300037@u-aizu.ac.jp>
2023-06-09 13:08:41 +0900

    Greetings added

--- dice.c
+++ dice.c
@@ -2,9 +2,8 @@
 #include<stdlib.h>
 
 int main(){
-    int a,b;
-    printf("Rolling dice...\n");
-    a = rand()%6 +1;
-    b = rand()%6 +1;
-    printf("Die 1: %d\nDie 2: %d\nTotal value: %d\n",a,b,a+b);
+    char name[10];
+    printf("What is your name?\n>");
+    scanf("%s",name);
+    printf("Hello, %s!",name);
 }
